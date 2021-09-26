5e76aad7a1a240663ee0516422429a640b26c722
ahoward1423 <89540666+ahoward1423@users.noreply.github.com>
2021-09-26 16:18:32 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PLAYER_NAME = os.getenv("PLAYER_NAME", default="Player One")
+print("Welcome", PLAYER_NAME, "to my Rock-Paper-Scissors game...")
+
 # game.py
 
 import random
@@ -14,6 +22,36 @@ print(user_choice)
 
 options = ["rock", "paper", "scissors"]
 
+# below if statement adapted from Gianna in Slack
+
+if user_choice not in options:
+    print ("Not a valid option, please play again")
+    exit()
+
 computer_choice = random.choice(options)
 print("Computer Chose: ")
 print(computer_choice)
+
+# worked with Arty on the below parts
+
+if computer_choice == user_choice:
+    print("You chose the same option, please pick again")
+elif user_choice == "rock":
+    if computer_choice == "scissors":
+        print ("Rock smashes scissors, congrats you won")
+    else:
+        print ("Paper covers rock, sorry you lost")
+elif user_choice == "scissors":
+    if computer_choice == "rock":
+        print ("Rock smashes scissors, sorry you lost")
+    else:
+        print ("Scissors cuts paper, congrats you won")
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print("Paper covers rock, congrats you won") 
+    else:
+        print("Scissors cuts paper, sorry you lost")
+
+
+
+
